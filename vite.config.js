8acf7ce5f37fd06985b227ef7b28ae0501b71799
Abdul Farzana <abdulfarzana143@gmail.com>
2026-06-12 16:23:07 +0530

    updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',   // your GitHub repo name
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCaseOnly",
-    },
-  },
+  base: '/My-portfolio/',
 })
